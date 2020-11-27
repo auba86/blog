8229f2a7d11405aa446a9d365e202d5987a8ad0e
auba86 <audrius.bareisis@gmail.com>
2020-11-27 19:30:39 +0200

    With last updates 2

--- src/test/java/blog/BlogApplicationTests.java
+++ src/test/java/blog/BlogApplicationTests.java
@@ -1,13 +1,34 @@
 package blog;
 
+import blog.model.Record;
+import blog.service.RecordService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static javafx.scene.input.KeyCode.L;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BlogApplicationTests {
 
+	private RecordService recordService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getRecordById(){
+		Record record = new Record();
+		record.setRecordId(50L);
+		record.setRecordTitle("Pirmas");
+		record.setRecordDescription("Labai gerai");
+		recordService.saveNewRecord(record);
+
+		Record record1 = recordService.getRecordById(record.getRecordId());
+
+		assertEquals("Pirmas", record1.getRecordTitle());
+
 	}
 
 }
--- src/test/java/blog/controller/RecordControllerTest.java
+++
@@ -1,7 +0,0 @@
-package blog.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RecordControllerTest {
-
-}
